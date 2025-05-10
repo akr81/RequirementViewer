8368bc87a9c9cd9e88a94555c2cd44db68328fdf
akr81 <akira21120903@gmail.com>
2025-05-10 18:57:24 +0900

    Update S&T

--- strategy_and_tactics.py
+++ strategy_and_tactics.py
@@ -3,9 +3,12 @@ from src.diagram_column import draw_diagram_column
 from src.operate_buttons import add_operate_buttons
 from src.diagram_configs import *
 from src.page_setup import initialize_page, load_and_prepare_data
+from src.utility import (
+    get_backup_files_for_current_data,
+    copy_file,
+)
 import uuid
 import copy
-import datetime
 
 
 color_list, config_data, demo, app_data, plantuml_process = initialize_page(
@@ -25,6 +28,7 @@ if data_key not in config_data:
     st.stop()
 
 file_path = config_data[data_key]
+st.session_state["file_path"] = file_path
 
 # データの読み込みと準備
 (
@@ -65,7 +69,20 @@ plantuml_code = draw_diagram_column(
 )
 
 with edit_column:
-    st.write("## データ編集")
+    title_column, file_selector_column = st.columns([4, 4])
+    with title_column:
+        st.write("## データ編集")
+    with file_selector_column:
+        # ファイル選択boxを表示
+        backup_files = get_backup_files_for_current_data()
+        st.selectbox(
+            "ファイルを選択",
+            backup_files,
+            0,
+            label_visibility="collapsed",
+            on_change=copy_file,
+            key="selected_backup_file",
+        )
     # 直接データ操作はせず、コピー(uuidは異なる)に対して操作する
     tmp_entity = copy.deepcopy(selected_entity)
     tmp_entity["unique_id"] = f"{uuid.uuid4()}".replace("-", "")
@@ -118,8 +135,6 @@ with edit_column:
         "type": "arrow",
     }
 
-    tmp_edges.append(new_edge)
-
     add_operate_buttons(
         selected_unique_id,
         tmp_entity,
@@ -128,6 +143,7 @@ with edit_column:
         id_title_dict,
         unique_id_dict,
         tmp_edges=tmp_edges,
+        new_edges=[new_edge],
     )
 
 # セッション状態にgraph_dataを追加
